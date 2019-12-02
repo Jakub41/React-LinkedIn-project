a3bf31d7f3632444e0c651905039d638feef717c
jakub41 <lemiszewski@gmx.com>
2019-12-02 14:10:52 +0100

    Fixed navigation to be horizontal

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,40 +1,60 @@
-import React, { useState } from 'react';
-import { FaLinkedin , FaSearch , FaHome ,  FaNetworkWired, FaSuitcase, FaArrowCircleDown} from 'react-icons/fa';
-import { IoIosNotificationsOutline,} from  "react-icons/io";
-import {TiMessages } from 'react-icons/ti';
-import { Input, Navbar , NavbarBrand , NavItem , NavLink,} from 'reactstrap';
+import React, { useState } from "react";
+import {
+  FaLinkedin,
+  FaSearch,
+  FaHome,
+  FaNetworkWired,
+  FaSuitcase,
+  FaArrowCircleDown
+} from "react-icons/fa";
+import { IoIosNotificationsOutline } from "react-icons/io";
+import { TiMessages } from "react-icons/ti";
+import { Input, Navbar, NavbarBrand, NavItem, NavLink } from "reactstrap";
 
-const NavBar = (props) => {
-    const [isOpen, setIsOpen] = useState(false);
-  
-    const toggle = () => setIsOpen(!isOpen);
-  
-    return (
-            <div>
-            <Navbar color="dark" light expand="md"></Navbar>
-            <NavbarBrand><FaLinkedin /></NavbarBrand>
-            <NavItem>
-            <NavLink href="/Home/"><FaHome /></NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink><FaNetworkWired /></NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink><FaSuitcase /></NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink><TiMessages /></NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink><IoIosNotificationsOutline /></NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink><FaArrowCircleDown /></NavLink>
-            </NavItem>
+const NavBar = props => {
+  const [isOpen, setIsOpen] = useState(false);
 
-                
-            </div>
-        );
-    }
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <>
+      <Navbar color="dark" light expand="md">
+        <NavbarBrand>
+          <FaLinkedin />
+        </NavbarBrand>
+        <NavItem>
+          <NavLink href="/Home/">
+            <FaHome />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <FaNetworkWired />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <FaSuitcase />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <TiMessages />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <IoIosNotificationsOutline />
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink>
+            <FaArrowCircleDown />
+          </NavLink>
+        </NavItem>
+      </Navbar>
+    </>
+  );
+};
 
 export default NavBar;

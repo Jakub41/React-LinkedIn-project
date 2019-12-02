a98d57f338d0c6fa0c67506a4971119375c87a00
jakub41 <lemiszewski@gmx.com>
2019-12-02 17:24:47 +0100

    API fix of the endpoint to get personal profile

--- src/Services/ProfileCRUD.js
+++ src/Services/ProfileCRUD.js
@@ -1,12 +1,12 @@
 import { createProfileAPIQUery } from "./BaseAPI";
-import { API_URL } from "./ApiUtils";
+import { API_URL, API_USERNAME } from "./ApiUtils";
 
 export const getAllProfiles = createProfileAPIQUery(() => {
   return `${API_URL}profile/`;
 });
 
 export const getProfile = createProfileAPIQUery(() => {
-  return `${API_URL}profile/me`;
+  return `${API_URL}profile/${API_USERNAME}`;
 });
 
 //calling, pass in body, and no other arguments are needed for 'params'

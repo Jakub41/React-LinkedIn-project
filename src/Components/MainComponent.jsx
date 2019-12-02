1fe6cb07ddae697f66190dfbee2a1692dbbb4dd4
nehavenkat <neha.veketsetty@gmail.com>
2019-12-02 14:03:03 +0100

    navbar

---
+++ src/Components/MainComponent.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import NavBar from './NavBar';
+
+class MainComponent extends Component {
+    render() {
+        return (
+            <div>
+            <NavBar />
+                
+            </div>
+        );
+    }
+}
+
+export default MainComponent;

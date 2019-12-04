22288b8a60db9adb5f87f2497d211b5348442cb8
jakub41 <lemiszewski@gmx.com>
2019-12-04 11:33:30 +0100

    Added a small API fix for ID

--- src/Services/ProfileCRUD.js
+++ src/Services/ProfileCRUD.js
@@ -18,9 +18,9 @@ export const addProfile = createProfileAPIQuery(() => {
 //calling, pass in body, and 'Id' will end up as part of 'params'
 //updateProfile(JSON.stringify(body), Id);
 export const updateProfile = createProfileAPIQuery(id => {
-  return `${API_URL}profile/${id}`;
+  return `${API_URL}profile/?id=${id}`;
 }, "PUT"); //id is used to specify whom we are updating
 
 export const deleteProfile = createProfileAPIQuery(id => {
-  return `${API_URL}profile/${id}`;
-}, "delete");
+  return `${API_URL}profile/?id=${id}`;
+}, "delete"); //id is used to specify whom we are deleting
